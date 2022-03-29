3024f2542d47c58dde06630fc568ccc1e2e6f6d5
Marcela Oliveira <mmarcela.oa@gmail.com>
2022-03-29 10:06:41 -0300

    iniciando projeto integrador

---
+++ src/main/java/br/com/r3connect/R3connectApplication.java
@@ -0,0 +1,13 @@
+package br.com.r3connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R3connectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R3connectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/r3connect/R3connectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.r3connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R3connectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
