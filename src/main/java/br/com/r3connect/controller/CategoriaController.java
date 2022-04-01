c0064d12464478b3d16054d7ef35b3c3812584fa
Marcela Oliveira <mmarcela.oa@gmail.com>
2022-04-01 10:24:23 -0300

    finalizando CRUD categoria

--- src/main/java/br/com/r3connect/controller/CategoriaController.java
+++ src/main/java/br/com/r3connect/controller/CategoriaController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +28,6 @@ public class CategoriaController {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 	
-	
 	@GetMapping
 	public ResponseEntity <List<Categoria>> getAll() {
 		return ResponseEntity.ok(categoriaRepository.findAll());
@@ -46,8 +46,7 @@ public class CategoriaController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Categoria> postCategoria(@Valid @RequestBody Categoria categoria){
-		
+	public ResponseEntity<Categoria> postCategoria(@Valid @RequestBody Categoria categoria){	
 		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
 	}
 	
@@ -56,7 +55,16 @@ public class CategoriaController {
 		return categoriaRepository.findById(categoria.getId())
 		.map(resposta -> ResponseEntity.ok().body(categoriaRepository.save(categoria)))
 		.orElse(ResponseEntity.notFound().build());
-		
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Object> deleteCategoria (@Valid @PathVariable Long id) {
+		return categoriaRepository.findById(id)
+				.map(resposta -> {
+					categoriaRepository.deleteById(id);
+					return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+				})
+				.orElse(ResponseEntity.notFound().build());
 	}
 
 }
