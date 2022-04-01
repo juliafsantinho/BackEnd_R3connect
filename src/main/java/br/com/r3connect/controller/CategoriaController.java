76260cdd59e2e92db860f52815b381ea191b6cdb
Matheus <matheusrabelosantos@hotmail.com>
2022-04-01 09:44:11 -0300

    Adicionando busca por ID e Material

--- src/main/java/br/com/r3connect/controller/CategoriaController.java
+++ src/main/java/br/com/r3connect/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,17 @@ public class CategoriaController {
 		return ResponseEntity.ok(categoriaRepository.findAll());
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity <Categoria> getById(@PathVariable Long id){
+		return categoriaRepository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@GetMapping("/material/{material}")
+	public ResponseEntity <List<Categoria>> getByMaterial(@PathVariable String material){
+		return ResponseEntity.ok(categoriaRepository.findAllByMaterialContainingIgnoreCase(material));
+	}
 	
 
 }
