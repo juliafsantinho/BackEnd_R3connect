f74da123c462631b6e71a6ed3f30e4fc8cbd9e75
Matheus <matheusrabelosantos@hotmail.com>
2022-04-04 09:57:44 -0300

    Adicionando model produto e interface produtoRepository

--- src/main/java/br/com/r3connect/model/Categoria.java
+++ src/main/java/br/com/r3connect/model/Categoria.java
@@ -1,19 +1,25 @@
 package br.com.r3connect.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categorias")
 public class Categoria {
 
-	//ATRIBUTOS
-	
+	//ATRIBUTOS	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -25,6 +31,10 @@ public class Categoria {
 	@NotNull(message = "O atributo descricao é obrigatório!")
 	@Size(min = 5, max = 1000, message = "O atributo Material deve possuir no mínimo 5 e no máximo 1000 caracteres.")
 	private String descricao;
+	
+	@OneToMany(mappedBy ="categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto>produto;
 
 	
 	public Long getId() {
@@ -49,6 +59,14 @@ public class Categoria {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+	
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
 	}
 	
 	
